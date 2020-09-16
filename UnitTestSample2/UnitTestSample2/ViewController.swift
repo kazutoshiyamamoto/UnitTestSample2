2fcc221469e1bdf65bf4ab3526121b5402b4cff6
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-09-16 16:42:30 +0900

    初回コミット

---
+++ UnitTestSample2/UnitTestSample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestSample2
+//
+//  Created by home on 2020/09/16.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
